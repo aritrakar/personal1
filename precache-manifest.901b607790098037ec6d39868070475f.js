e280cba7be77991932eb168a56beb4c0f6c523df
Aritra Kar <aritrakar2002@gmail.com>
2020-08-25 11:04:18 +0530

    Updates

---
+++ precache-manifest.901b607790098037ec6d39868070475f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3722c54c81ff14bba5c0e7331539394e",
+    "url": "/aritrakar/personal-website-v1/index.html"
+  },
+  {
+    "revision": "cc9eb4b372e9ecfc9b24",
+    "url": "/aritrakar/personal-website-v1/static/css/main.91d73110.chunk.css"
+  },
+  {
+    "revision": "a8a826a6d53edc40fe5e",
+    "url": "/aritrakar/personal-website-v1/static/js/2.f41bd186.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aritrakar/personal-website-v1/static/js/2.f41bd186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9eb4b372e9ecfc9b24",
+    "url": "/aritrakar/personal-website-v1/static/js/main.fe46e0b1.chunk.js"
+  },
+  {
+    "revision": "c3e608666d4850e6c0a0",
+    "url": "/aritrakar/personal-website-v1/static/js/runtime-main.fbc85548.js"
+  },
+  {
+    "revision": "c5f6fb44656c1ed9f08014737010f387",
+    "url": "/aritrakar/personal-website-v1/static/media/IMG_0115.c5f6fb44.JPG"
+  },
+  {
+    "revision": "d63e1427ebfc7fb233a8f57ffc674f2e",
+    "url": "/aritrakar/personal-website-v1/static/media/aritra.d63e1427.png"
+  },
+  {
+    "revision": "f138887712d87928e3f7586e6dc4b75b",
+    "url": "/aritrakar/personal-website-v1/static/media/gear.f1388877.png"
+  }
+]);
