a6fbe11f46aea2228016df4f7899877d0f180bfb
Aritra Kar <aritrakar2002@gmail.com>
2020-08-25 19:33:34 +0530

    Updated projectsData.js

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,44 +1,57 @@
 const items = [
-    {
-        "id": 1,
-        "Name": "WhatsApp Chatbot v1",
-        "Description": "A simple chatbot that can respond to basic greetings and conversation hooks, fetch YouTube video links, news and Wikipedia articles.",
-        "Used": "Python, Selenium, BeautifulSoup",
-        "Link": "https://github.com/aritrakar/Web-Scraping-Projects/tree/master/Chatbot",
-        "Path": ""
-    },
-    {
-        "id": 2,
-        "Name": "To Do App",
-        "Description": "First React.js project. Simple To-Do app.",
-        "Used": "HTML, CSS, JavaScript, React.js",
-        "Link": "https://github.com/aritrakar/JS/tree/master/To%20Do",
-        "Path": ""
-    },
-    {
-        "id": 3,
-        "Name": "ColorPicker",
-        "Description": "Generates a random background color (RGB or Hex) and sets the color accordingly. Simple JS exercise.",
-        "Used": "HTML, CSS, JavaScript",
-        "Link": "https://github.com/aritrakar/JS/tree/master/ColorPicker",
-        "Path": "ColorPicker/index.html"
-    },
-    {
-        "id": 4,
-        "Name": "Pong",
-        "Description": "An attempt to recreate the classic game. See if you can beat the computer!",
-        "Used": "HTML, CSS, JavaScript",
-        "Link": "https://github.com/aritrakar/JS/tree/master/pong",
-        "Path": "Pong/index.html"
-    },
-    {
-        "id": 5,
-        "Name": "Bouncing Balls",
-        "Description": "Simple single-player game to \"devour\" the moving balls in the least time.",
-        "Used": "HTML, CSS, JavaScript",
-        "Link": "https://github.com/aritrakar/JS/tree/master/bouncing-balls",
-        "Path": ""
-    }
-]
+  {
+    id: 1,
+    Name: "WhatsApp Chatbot v1",
+    Description:
+      "A simple chatbot that can respond to basic greetings and conversation hooks, fetch YouTube video links, news and Wikipedia articles.",
+    Used: "Python, Selenium, BeautifulSoup",
+    Link:
+      "https://github.com/aritrakar/Web-Scraping-Projects/tree/master/Chatbot",
+    Path: "",
+  },
+  {
+    id: 2,
+    Name: "Pathfinding Visualizer",
+    Description: "Inspired by Clement Mihailescu.",
+    Used: "HTML, CSS, JavaScript, React.js",
+    Link: "https://aritrakar.github.io/pathfinding_visualizer/",
+    Path: "",
+  },
+  {
+    id: 3,
+    Name: "To Do App",
+    Description: "First React.js project. Simple To-Do app.",
+    Used: "HTML, CSS, JavaScript, React.js",
+    Link: "https://github.com/aritrakar/JS/tree/master/To%20Do",
+    Path: "",
+  },
+  {
+    id: 4,
+    Name: "ColorPicker",
+    Description:
+      "Generates a random background color (RGB or Hex) and sets the color accordingly. Simple JS exercise.",
+    Used: "HTML, CSS, JavaScript",
+    Link: "https://github.com/aritrakar/JS/tree/master/ColorPicker",
+    Path: "ColorPicker/index.html",
+  },
+  {
+    id: 5,
+    Name: "Pong",
+    Description:
+      "An attempt to recreate the classic game. See if you can beat the computer!",
+    Used: "HTML, CSS, JavaScript",
+    Link: "https://github.com/aritrakar/JS/tree/master/pong",
+    Path: "Pong/index.html",
+  },
+  {
+    id: 6,
+    Name: "Bouncing Balls",
+    Description:
+      'Simple single-player game to "devour" the moving balls in the least time.',
+    Used: "HTML, CSS, JavaScript",
+    Link: "https://github.com/aritrakar/JS/tree/master/bouncing-balls",
+    Path: "",
+  },
+];
 
-export default items
+export default items;
